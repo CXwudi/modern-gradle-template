99bdfed3c512278592b24397a848ae753a07d9dc
CXwudi <cx2010017@gmail.com>
2022-11-05 05:52:56 -0400

    cleanup

--- gradle/plugins/spring-plugins/kotlin-jvm/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-  `kotlin-dsl` // !id("kotlin-dsl"), this tells gradle to use kotlin dsl
-}
-
-dependencies {
-  implementation(platform("poc.cx.glp:version-constraints"))
-  implementation(project(":spring-plugins:java"))
-  implementation(project(":kotlin-jvm-plugins"))
-}
