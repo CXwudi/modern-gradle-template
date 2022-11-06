0616cd3fa0e5760bbfb1e429b83aeab049a6feeb
CXwudi <cx2010017@gmail.com>
2022-11-06 02:00:53 -0500

    :memo: update doc

--- build-logic/plugins/mixin/spring-boot-app/src/main/kotlin/my.spring-boot-app-mixin.gradle.kts
+++ build-logic/plugins/mixin/spring-boot-app/src/main/kotlin/my.spring-boot-app-mixin.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-  // so far this plugin just add
+  // so far this plugin just adds
   id("org.springframework.boot")
   // optional, can be used to distribute build scripts for click and run.
   // but need to set applications.mainClass explicitly in build.gradle.kts
-  // it's better to app this plugin at the main spring app build who really needs it.
+  // it's better to add this plugin at the main spring app build who really needs it.
   // id("my.app-mixin")
 }
