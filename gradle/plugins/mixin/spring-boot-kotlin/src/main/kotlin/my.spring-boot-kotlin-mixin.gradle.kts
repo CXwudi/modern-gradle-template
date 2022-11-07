a194f02c43e81a51eb45df4bea667d12685d557b
CXwudi <cx2010017@gmail.com>
2022-11-07 00:14:14 -0500

    :memo: more doc

--- gradle/plugins/mixin/spring-boot-kotlin/src/main/kotlin/my.spring-boot-kotlin-mixin.gradle.kts
+++ gradle/plugins/mixin/spring-boot-kotlin/src/main/kotlin/my.spring-boot-kotlin-mixin.gradle.kts
@@ -7,4 +7,6 @@ dependencies {
   implementation("com.github.CXwudi:kotlin-jvm-inline-logging")
   testImplementation("io.kotest.extensions:kotest-extensions-spring")
   testImplementation("org.mockito.kotlin:mockito-kotlin")
+  // due to https://github.com/Ninja-Squad/springmockk#limitations, mockk and springmockk are not added by default here,
+  // use my.spring-boot-mockk-mixin instead
 }
