136441a34df6c5bb48b8fd8cd6ae92fa550eecd2
CXwudi <cx2010017@gmail.com>
2022-11-08 00:42:39 -0500

    :zap: simplify include

--- gradle/plugins/settings.gradle.kts
+++ gradle/plugins/settings.gradle.kts
@@ -15,31 +15,19 @@ dependencyResolutionManagement {
 
 // current grouping strategy: each script plugin is a separate submodule to maximize parallelism
 
-// the root of all plugins, configure the jvm
+// first, include the root of all plugins, only configure the jvm
 include("root")
 
-// the core mixins, used by the conventions mentioned below
-// the main build should try to avoid applying these mixins directly but prefer conventions first
-include("mixin:app")
-include("mixin:lib")
-include("mixin:junit5")
-include("mixin:lombok")
-include("mixin:kotlin-jvm")
-include("mixin:kotest-jvm")
-include("mixin:spring-boot")
-include("mixin:spring-boot-app")
-include("mixin:spring-boot-kotlin")
+val toResolvedDirectories = listOf(
+  // the core mixins and the optional mixins used by the conventions mentioned below
+  // the main build should prefer conventions first, then use some optional mixins
+  "mixin",
+  // the conventions, main builds should depend on these first
+  "convention"
+)
 
-// the optional mixins, main builds can optionally apply these plugins
-include("mixin:spring-boot-appfix")
-include("mixin:spring-boot-mockk")
-
-// the conventions, main builds should depend on these first
-include("convention:java-app")
-include("convention:java-lib")
-include("convention:kotlin-app")
-include("convention:kotlin-lib")
-include("convention:spring-app")
-include("convention:spring-lib")
-include("convention:kotlin-spring-app")
-include("convention:kotlin-spring-lib")
+toResolvedDirectories.forEach { dir ->
+  rootDir.resolve(dir).listFiles { file -> file.isDirectory && !file.isHidden }?.forEach {
+    include("$dir:${it.name}")
+  }
+}
