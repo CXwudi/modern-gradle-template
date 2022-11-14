3cbbe6ab2cd536fa4e63b5ed6ed80124f80d602f
CXwudi <cx2010017@gmail.com>
2022-11-13 22:10:43 -0500

    :white_check_mark: optimized start script

--- gradle/plugins/mixin/app/src/main/kotlin/my.app-mixin.gradle.kts
+++ gradle/plugins/mixin/app/src/main/kotlin/my.app-mixin.gradle.kts
@@ -4,6 +4,13 @@ plugins {
 }
 
 application {
-  // we don't want run scripts in bin folder, but at the root of folder
+  // we don't want the run scripts in bin folder, but at the root folder
   executableDir = ""
+}
+
+tasks.named<org.gradle.jvm.application.tasks.CreateStartScripts>("startScripts") {
+  // use wildcard to include all jars in lib folder,
+  // instead of default behavior of listing each jar name and version explicitly,
+  // so that this enables hot swapping of jars in lib folder
+  classpath = files("lib/*")
 }
