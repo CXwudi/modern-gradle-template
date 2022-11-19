7d4e1cf5f73d11ab3a03d2747613d2f2be69676f
CXwudi <cx2010017@gmail.com>
2022-11-19 11:34:52 -0500

    :truck: better naming in version catalog

--- gradle/platform/version-constraints/build.gradle.kts
+++ gradle/platform/version-constraints/build.gradle.kts
@@ -18,25 +18,23 @@ dependencies {
 
   // benefit from spring boot's version management for non-spring projects
   // see https://docs.spring.io/spring-boot/docs/current/reference/html/dependency-versions.html to check what lib is included
-  api(platform(libs.spring.boot.dependencies))
+  api(platform(libs.bom.spring.boot.dependencies))
   // api(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.4")) // enable if spring screws up coroutines
-  api(platform(libs.kotest.bom))
+  api(platform(libs.bom.kotest))
   constraints {
     // add your api() here to manage dependencies not defined in bom or gradle platform
     // plugins dependencies, be used in gradle/plugins build.gradle.kts
-    api(libs.gradle.versions.plugin)
-    api(libs.gradle.version.catalog.update.plugin)
-    api(libs.kotlin.gradle.plugin)
-    api(libs.kotlin.allopen)
-    api(libs.spring.boot.gradle.plugin)
-    api(libs.spring.dependency.management.plugin)
-    api(libs.lombok.gradle.plugin)
+    api(libs.plugin.kotlin)
+    api(libs.plugin.kotlin.allopen)
+    api(libs.plugin.spring.boot)
+    api(libs.plugin.spring.dependency.management)
+    api(libs.plugin.freefair.lombok)
 
     // main build dependencies, be used in the actual app and lib build.gradle.kts
-    api(libs.kotlin.jvm.inline.logging)
-    api(libs.kotest.extensions.spring)
-    api(libs.mockito.kotlin)
-    api(libs.mockk)
-    api(libs.spring.mockk)
+    api(libs.dep.kotlin.jvm.inline.logging)
+    api(libs.dep.kotest.extensions.spring)
+    api(libs.dep.mockito.kotlin)
+    api(libs.dep.mockk)
+    api(libs.dep.spring.mockk)
   }
 }
