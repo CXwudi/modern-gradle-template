d51fc11bf2181dbbe0f9359df9cca2d9c2e4a142
CXwudi <cx2010017@gmail.com>
2022-12-03 11:39:34 -0500

    :zap: add spring native compile

--- sample-kotlin-spring-app/build.gradle.kts
+++ sample-kotlin-spring-app/build.gradle.kts
@@ -1,7 +1,9 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
   id("my.kotlin-spring-app")
   id("my.app-mixin") // demonstrating using the app-mixin plugin to generate convenience run scripts
 //  id("my.spring-boot-mockk-mixin") // u can add other mixins if you want
+  alias(libs.plugins.graalvm.native.image) // demonstrating using plugin directly from version catalog
 }
 
 dependencies {
