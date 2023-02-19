d78b94b00b0cc97ae23286b4117dd826cd54382d
CXwudi <cx2010017@gmail.com>
2023-02-19 12:09:53 -0500

    upgrade to gradle 8.0.1

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-  // this convention plugin must stay at the top-level build script, not in gradle/plugins
-  // an experimental gradle wrapper with JDK downloaded
-//  id("me.filippov.gradle.jvm.wrapper") version "0.14.0"
-}
-
-// jvmWrapper {
-//  linuxX64JvmUrl = "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.5%2B8/OpenJDK17U-jdk_x64_alpine-linux_hotspot_17.0.5_8.tar.gz"
-// }
