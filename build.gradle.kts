f49bdd10a5dcfb1cbd9ed6fdd201c0ebdf73d028
CXwudi <cx2010017@gmail.com>
2023-04-17 14:58:12 -0400

    successfully make self-updating working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,38 @@
-// this root build.gradle.kts is only used by the workaround below
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
   // if you don't want to do set kotlin.pluginLoadedInMultipleProjects.ignore=true in gradle.properties file,
   // then do https://youtrack.jetbrains.com/issue/KT-46200/False-positive-for-The-Kotlin-Gradle-plugin-was-loaded-multiple-times-when-applied-kotlin-and-jvm-plugins-in-different-modules#focus=Comments-27-4850274.0-0
   alias(libs.plugins.kotlin.jvm) apply false
+  // }
+  // if you rely on renovatebot to update your dependencies, then you don't need these plugins
+  // and u can delete everything below here
+  alias(libs.plugins.ben.manes.versions)
+  alias(libs.plugins.version.catalog.update)
+}
+
+fun isNonStable(version: String): Boolean {
+  val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+  val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+  val isStable = stableKeyword || regex.matches(version)
+  return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+  rejectVersionIf {
+    isNonStable(candidate.version)
+  }
+  gradleReleaseChannel = "current"
+}
+
+versionCatalogUpdate {
+  sortByKey.set(false)
+  keep {
+    // keep versions without any library or plugin reference
+    keepUnusedVersions.set(true)
+    // keep all libraries that aren't used in the project
+    keepUnusedLibraries.set(true)
+    // keep all plugins that aren't used in the project
+    keepUnusedPlugins.set(true)
+  }
 }
