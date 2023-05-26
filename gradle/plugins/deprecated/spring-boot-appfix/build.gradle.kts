3c3ccf9327e427a98c5e7edc1a9bfa3174098915
CXwudi <cx2010017@gmail.com>
2023-05-26 14:37:13 -0400

    :wastebasket: deleted deprecated stuff

--- gradle/plugins/deprecated/spring-boot-appfix/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-  `kotlin-dsl` // !id("kotlin-dsl"), this tells gradle to use kotlin dsl
-}
-
-dependencies {
-  implementation(project(":root"))
-  implementation("org.springframework.boot:spring-boot-gradle-plugin")
-}
--- gradle/plugins/deprecated/spring-boot-mockk/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-  `kotlin-dsl` // !id("kotlin-dsl"), this tells gradle to use kotlin dsl
-}
-
-dependencies {
-  implementation(project(":root"))
-}
